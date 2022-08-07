cec797d6db322a825e88a9794380bb83fa79c5f5
黎林峰 <1491541200@qq.com>
2022-08-08 00:10:14 +0800

    change2

--- src/main.js
+++ src/main.js
@@ -4,18 +4,22 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import FastClick from "fastclick"
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 // 解决浏览器300毫秒延迟
 FastClick.attach(document.body)
-/* eslint-disable no-new */
+Vue.use(VueAwesomeSwiper)
+    /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,22 +1,21 @@
 <template>
-<div>
-   <home-header></home-header>
-</div>
+  <div>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+    44
+  </div>
 </template>
 
 <script>
-
-
-import HomeHeader from './components/Header.vue'
+import HomeHeader from "./components/Header.vue";
+import HomeSwiper from "./components/Swiper.vue";
 
 export default {
-   name:'Home',
-   components:{
+  name: "Home",
+  components: {
     HomeHeader
-   }
-}
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
