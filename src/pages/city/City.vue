5e26b35da301cd83b33b185a40fb08f8df7db99a
黎林峰 <1491541200@qq.com>
2022-08-11 11:47:28 +0800

    gg1

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,13 +1,21 @@
 <template>
+<div>
 <city-header></city-header>
-<!-- <div>开心</div> -->
+<city-search></city-search>
+</div>
+
+
+
 </template>
 <script>
 import CityHeader from './components/Header.vue'
+import CitySearch from './components/Search.vue'
+
 export default {
    name:'City',
    components:{
-    CityHeader
+    CityHeader,
+    CitySearch
    }
 }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="search">
+    <input   class="search-input" type="text" placeholder="输入城市名或拼音" 
+    >
+</div>
+</template>
+<script>
+export default {
+   name:'CitySearch'
+}
+</script>
+ <style   lang="stylus"  scoped>
+@import '~@/assets/styles/varibles.styl'
+ .search
+    height: 0.72rem
+    padding: 0 .1rem
+    background: $bgColor
+    .search-input
+      box-sizing: border-box
+      box-sizing: content-box
+      width: 100%
+      padding: 0 .1rem
+      line-height: 0.62rem
+      text-align: center
+      border-radius: 0.06rem
+      color: #666
+ </style>
